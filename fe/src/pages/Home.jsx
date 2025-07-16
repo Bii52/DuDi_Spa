ab4d8e9c8b9c21521eea31c4278f7ddb3954d24a
Natori <longvo1903@gmail.com>
2025-07-16 12:53:28 +0700

    Service Request Form

---
+++ fe/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import ServiceRequest from "../components/ServiceRequest";
+
+const Home = () => {
+
+  return (
+    <div class="bg-orange-100 flex h-fit w-screen items-center justify-center">
+      <ServiceRequest/>
+    </div>
+  )
+}
+
+export default Home;
