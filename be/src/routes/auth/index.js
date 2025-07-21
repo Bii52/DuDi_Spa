0990ec09b309092ca13f860bc7be992b84ee3efe
DUDISoftware <softwaredudi@gmail.com>
2025-07-21 13:09:41 +0700

    fix full

--- be/src/routes/auth/index.js
+++ be/src/routes/auth/index.js
@@ -4,18 +4,6 @@ import verifyToken from "../../middleware/verify_token.middleware.js";
 
 const router = Router();
 
-router.post("/test-verify-token", verifyToken, (req, res) => {
-  console.log(req.userData)
-  return res.status(200).json({
-    "error": 0,
-    "error_text": "",
-    "data_name": "Xác thực thành công",
-    "data": {
-      "user": req.userData.user
-    }
-  });
-});
-
 // Register route
 router.post("/register", registerController);
 // Login route
--- be/src/routes/index.js
+++ be/src/routes/index.js
@@ -3,9 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 import authRouter from "./auth";
-import serviceRouter from "./service";
 
 router.use("/auth", authRouter);
-router.use("/service", serviceRouter);
 
 export default router;
