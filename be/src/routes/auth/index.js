254096904f0cc74c391c8cfd788497abdba90a32
DUDISoftware <softwaredudi@gmail.com>
2025-07-21 13:19:15 +0700

    ok

--- be/src/routes/auth/index.js
+++ be/src/routes/auth/index.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { registerController, loginController, logoutController } from "../../controllers/authenticate.controller.js";
-import verifyToken from "../../middleware/verify_token.middleware.js";
 
 const router = Router();
 
