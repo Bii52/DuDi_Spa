9f5454203a8c3882b28a461d995fe8284f2f6c02
thu tam <ngocvotam27@gmail.com>
2025-07-24 09:09:33 +0700

    fe booking, detail, popup

--- fe/src/routes/Routes.jsx
+++ fe/src/routes/Routes.jsx
@@ -4,15 +4,30 @@ import Home from '../pages/Home';
 import Contact from '../pages/Contact';
 import Profile from '../pages/Profile';
 import MainLayout from '../Layout/Layout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import AuthLayout from '../Layout/AuthLayout';
 import About from '../pages/About';
+import ServiceDetail from '../pages/ServiceDetail';
+import Booking from '../pages/Booking';
+
+
+
 const AppRoutes = () => (
   <Routes>
+        <Route element={<AuthLayout/>}>
+        < Route path='/login' element={<Login/>} />
+        < Route path='/register' element={<Register/>} />
+      </Route>
+
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} /> {/* "/" sẽ load Home */}
       <Route path="home" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       <Route path="profile" element={<Profile />} />
+      < Route path='/services/:id' element={<ServiceDetail/>} />
+      < Route path='/booking/:id' element={<Booking/>} />
     </Route>
     <Route path="*" element={<div className="text-center p-10">404 Not Found</div>} />
   </Routes>
